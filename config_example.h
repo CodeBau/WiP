5fef6cada4f1c1c31f756fd2479ccd49b7cd5e62
Marcin Lesniewski <lesniakmeister@gmail.com>
2021-02-02 21:54:03 +0100

    files sorted into folders

--- config_example.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-const char DbHost[]{ "localhost" };
-const char DbUser[]{ "root" };
-const char DbPsw[]{ "passwd" };
-const char DbName[]{ "database" };
--- logs.h
+++
@@ -1,7 +0,0 @@
-#ifndef LOGS_H
-#define LOGS_H
-#include <QString>
-
-void mylog(QString log_text);
-
-#endif // LOGS_H
